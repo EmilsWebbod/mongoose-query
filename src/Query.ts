3ec6f5f69aa0d6896ef2f90011ac1d562ddbf89b
EmilsWebbod <emil@webbod.no>
2023-06-11 10:10:52 +0200

    Strip empty strings for search

--- src/Query.ts
+++ src/Query.ts
@@ -293,6 +293,7 @@ export class Query<
     for (const key in query) {
       if (query.hasOwnProperty(key)) {
         const str = String(query[key]);
+        if (!str) continue;
         if (key[0] === '$') {
           if (typeof query[key] !== 'string') continue;
           const { field, value } = mongooseQueryWithOperation(
@@ -300,7 +301,7 @@ export class Query<
             key,
             str
           );
-          if (field) {
+          if (field && value) {
             operationQuery[field] = value;
           }
           delete query[key];
